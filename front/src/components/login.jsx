3060a20c4aa97e5f578a67b0388d48416cb2e254
Hyosun Lee <hyosunlee@Hyosuns-MacBook-Pro.local>
2022-11-07 17:35:01 +0000

    change for the userName and function to find right user to login

--- front/src/components/login.jsx
+++ front/src/components/login.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { useNavigate } from "react-router-dom";
-import { client } from "../client";
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import { client } from '../client';
 
 export default function Login() {
   const [guests, setGuests] = useState(null);
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,47 +23,43 @@ export default function Login() {
   }, []);
 
   // map over guests and get userName and password
-  const guestUserName = guests?.map((guest) => guest.userName);
-  const guestPassword = guests?.map((guest) => guest.password);
   console.log(guests);
-
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (guestUserName === userName && guestPassword === password) {
-      navigate("/home");
-    }
-    else {
-      alert("Wrong username or password");
-    }
+    let guest = guests.find(
+      (guest) =>
+        guest.userName.current === userName &&
+        guest.password.current === password
+    );
+    console.log(guest);
+    guest ? navigate('/home') : alert('Wrong username or password');
   }
 
   return (
-    
-    <div className="Login">
+    <div className='Login'>
       <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="userName">
+        <Form.Group size='lg' controlId='userName'>
           <Form.Label>Username</Form.Label>
           <Form.Control
             autoFocus
-            type="text"
+            type='text'
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
           />
         </Form.Group>
-        <Form.Group size="lg" controlId="password">
+        <Form.Group size='lg' controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
+            type='password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit">
+        <Button block size='lg' type='submit'>
           Login
         </Button>
       </Form>
-
     </div>
   );
 }
