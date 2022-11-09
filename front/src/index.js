93f422b6e4df5e1bbc4b194dee0665cda067574a
Hyosun Lee <hyosunlee@Hyosuns-MacBook-Pro.local>
2022-11-09 20:15:00 +0000

    explore the structure for better understanding

--- front/src/index.js
+++ front/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter  basename={process.env.PUBLIC_URL}>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-
-
--- front/src/pages/loginPage/index.jsx
+++ front/src/pages/loginPage/index.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import Login from "../../components/login";
-import DDay from "../../components/DDAY/index";
-import "./index.scss";
+import React from 'react';
+import Login from '../../components/login';
+import DDay from '../../components/DDAY/index';
+import './index.scss';
 
 const LoginPage = () => {
   return (
-    <div className="loginPage">
-      <div className="loginPage_Form">
+    <div className='loginPage'>
+      <div className='loginPage_Form'>
         <h1>SAVE THE DATE</h1>
         <div className='loginPage_Content'>
-        <h2>Maëva & Julien</h2>
-        <p>
-          Nous sommes impatients de célébrer notres mariage à vos côtés le
-          samedi 20 mai 2023. En attendant de pouvoir communiquer tous les
-          détails, nous vous invitons à répondre au préalable à quelques
-          questions.
-        </p>
-        <DDay />
-        <Login />
-      </div></div>
+          <h2>Maëva & Julien</h2>
+          <p>
+            Nous sommes impatients de célébrer notres mariage à vos côtés le
+            samedi 20 mai 2023. En attendant de pouvoir communiquer tous les
+            détails, nous vous invitons à répondre au préalable à quelques
+            questions.
+          </p>
+          <DDay />
+          <Login />
+        </div>
+      </div>
     </div>
   );
 };
