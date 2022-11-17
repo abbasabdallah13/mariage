775f1be03838ff6d776c2ab096fa1fecad1e9dba
Julien Sarda <104718280+JUJUdev1@users.noreply.github.com>
2022-11-17 13:39:12 +0100

    added the content of dashboard page with some explanation and temporary css

---
+++ front/src/components/dashboardHeader/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './index.scss'
+
+const DashboardHeader = () => {
+  return (
+    <div className='dashboard-container'>
+        <h1>
+            Dashboard
+        </h1>
+        <h2>
+            admin name here
+        </h2>
+    </div>
+  )
+}
+
+export default DashboardHeader
--- front/src/pages/dashboardPage/index.jsx
+++ front/src/pages/dashboardPage/index.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import DashboardHeader from '../../components/dashboardHeader/index';
+import DashboardNav from '../../components/dashboardNav/index';
+
+import './index.scss'
+
+
 
 const DashboardPage = () => {
-
   const localUserName = localStorage.getItem("userName");
 
   const Navigate = useNavigate();
-  
+
   useEffect(() => {
     !localUserName && Navigate("/loginDashboard");
   }, [localUserName]);
 
-  return <div>DashboardPage</div>;
+  return (
+    <div className="dashboard-page">
+      <DashboardHeader />
+      <div className="dashboard-content">
+        The idea with this is to fetch the info list from the anwser , then in guest list we can see the list of guests and in the dashboard we can see the list of the answers
+        so when we click on the guest we can see the answers of the guest
+        
+      </div>
+      <DashboardNav />
+    </div>
+  );
 };
 
 export default DashboardPage;
